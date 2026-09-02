5842ec1739907e25daf14cea2f0e023417ebf084
mayankpathar <mayankpathar49@gmail.com>
2026-09-02 08:20:19 +0530

    Refactor code for improved readability and fix logic for counting consecutive 1s

--- array/Easy/a1.c
+++ array/Easy/a1.c
@@ -1,34 +1,34 @@
 // Given a binary array nums, return the maximum number of consecutive 1s in the array.
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int M,count=1,k=1;
+int main()
+{
+    int M, count = 0, k = 0;
     printf("enter the value of arr size:");
-    scanf("%d",&M);
+    scanf("%d", &M);
     int nums[M];
     for (int i = 0; i < M; i++)
     {
-       printf("enter the value of arr element: ");
-       scanf("%d",&nums[i]);
+        printf("enter the value of arr element: ");
+        scanf("%d", &nums[i]);
     }
-   for (int i = 0; i < M; i++)
-   {
-    if (1==nums[i]==nums[i+1])
+    for (int i = 0; i < M; i++)
     {
-        count++;
-       
+        if (nums[i] == 1)
+        {
+            count++;
+            if (count > k)
+            {
+                k++;
+            }
+        }
+        else
+        {
+            count = 0;
+        }
     }
-    if (count>=k)
-    {
-       k= count;
-    }
-    count=1;
-    
-   }
-   
-   
-   printf("%d",k);
-   
+
+    printf("%d", k);
 
     return 0;
 }
